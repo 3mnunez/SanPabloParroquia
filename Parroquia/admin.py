4e562fbba2967024726580d7fc40a67a9bdc7d17
Marco <untalmarco8@gmail.com>
2017-07-19 17:39:37 -0600

    Primer commit

---
+++ Parroquia/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Video, Album, Imagen, Donacion
+
+admin.site.register(Video)
+admin.site.register(Album)
+admin.site.register(Imagen)
+admin.site.register(Donacion)
+
+
+# @admin.register(Album)
+# class AlbumAdmin(admin.ModelAdmin):
+# 	list_display = ('name', 'artist',)
+
